0008e35cede7c76d60ccfb7d3a3a42ebfd2ccef2
Ahmet <Nyahqul@gmail.com>
2019-10-13 14:24:16 +0300

    SpringBootApplication and Schema.sql

---
+++ sampleExProject/src/main/java/com/sampleExProject/Application/SampleExProjectApp.java
@@ -0,0 +1,13 @@
+package com.sampleExProject.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleExProjectApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SampleExProjectApp.class, args);
+	}
+}
